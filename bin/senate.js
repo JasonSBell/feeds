75eddb301332ba85557e781c10bb8527f23b6bb5
Jason Bell <jsbell9@gmail.com>
2022-04-19 18:49:24 -0500

    Added scripts for scraping congressional trades.

---
+++ bin/senate.js
@@ -0,0 +1,37 @@
+const log4js = require("log4js");
+
+const fetchSenateTrades = require("../lib/senate");
+const event = require("../lib/event");
+const logger = require("../lib/logger");
+
+async function main() {
+  fetchSenateTrades()
+    .then((items) => {
+      logger.info(`received ${items.length} records`);
+
+      return Promise.allSettled(
+        items.map((item) =>
+          event.publish({
+            timestamp: new Date(),
+            name: "congressional_trade",
+            source: "feeds",
+            body: item,
+          })
+        )
+      );
+    })
+    .then((results) => {
+      results.forEach((result) => {
+        if (result.status === "rejected") {
+          logger.error(`Failed to publish event ${result.reason}`);
+        } else {
+          logger.info(`Published event ${result.value.id}`);
+        }
+      });
+    })
+    .finally(() => {
+      log4js.shutdown();
+    });
+}
+
+main();
